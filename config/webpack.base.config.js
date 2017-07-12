d544ce10b4848e9c054f82a1d7b60fbfd4e7d633
shinseed <307610991@qq.com>
2017-07-12 14:54:34 +0800

    webpack init

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,9 +1,39 @@
 var path = require('path');
-
+var utils = require('./utils')
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
-  }
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': resolve('src')
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: [resolve('src')]
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: utils.assetsPath('img/[name].[hash:7].[ext]')
+            }
+        }, {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+            }
+        }]
+    }
 };
