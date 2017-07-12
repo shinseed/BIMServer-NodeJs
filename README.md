1ddd1a1b54f76dfdd277a075b873051203ca3789
shinseed <307610991@qq.com>
2017-07-12 16:52:23 +0800

    注释

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Three.js-WebGl-View
 基于Three.js-WebGl-View
 
-#使用
+# 使用
 1.npm install || yarn install
 
 2.npm run dev 
 
-#文档目录
+# 文档目录
 src 源码
 sliceJson 把大的模型文件切割成小模型
 static 静态资源 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 import ThreeBase from '~/webgl/js/ThreeInterface'
 import $ from 'jquery'
 
-window.onload = function() {
-
-
+/**
+ * 初始化webgl
+ */
+function webglInit() {
+    //初始化 canvas 的宽高
     let state = {
-            webglstyle: {
-                width: window.innerWidth,
-                height: window.innerHeight
-            }
+        webglstyle: {
+            width: window.innerWidth,
+            height: window.innerHeight
         }
-        //容器要和内部canvas成比例 
-    $('#webgl').attr('style', `width:${state.webglstyle.width}px;height:${state.webglstyle.height}px`)
+    }
+
+    //容器要和内部canvas成比例 
+    $('#webgl').attr('style', `width:${state.webglstyle.width}px;height:${state.webglstyle.height-20}px`)
     window.webgl = new ThreeBase('webgl', state);
+}
+
+/**
+ * 创建 加载文件的数组对象
+ * @return {[array]} array
+ */
+function createLoadModelInfo() {
     let arr = [];
     for (let i = 1; i < 22; i++) {
         arr.push({
@@ -33,20 +43,38 @@ window.onload = function() {
             }
 
         })
-
-
     }
+    return arr;
+}
+
+/**
+ * 加载模型 
+ * @param  {[array]} arr [加载模型的数组对象]
+ */
+function loadModel(arr) {
     window.webgl.LoadJsonArr({
         data: arr,
         state: true
     }, function(callback) {
+        //加载进度信息
         console.log(callback)
     })
+}
+/**
+ * dom完成后 初始化
+ */
+window.onload = function() {
 
+    webglInit();
 
-      $('#superMario').on('click',function(){
+    loadModel(createLoadModelInfo());
 
+    //监听 超级玛丽按钮
+    $('#superMario').on('click', function() {
+
+        //开启超级玛丽模式
         window.webgl.SuperMarioStart();
-         $('#superMario').blur()//失去焦点
+        $('#superMario').blur() //失去焦点
+        alert('w、a、s、d 空格 ps:只做了向下的碰撞检测')
     })
 }
