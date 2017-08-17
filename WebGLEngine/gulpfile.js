b4d1c05a5c174d56db47cfcf274f320163b7eb88
shinseed <307610991@qq.com>
2017-08-17 14:27:17 +0800

    webgl engine cli

---
+++ WebGLEngine/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var watchify = require("watchify");
+var tsify = require("tsify");
+var gutil = require("gulp-util");
+var paths = {
+    pages: ['examples/*.html']
+};
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/cyclegl.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('cyclegl.js'))
+        .pipe(gulp.dest("dist"));
+}
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
