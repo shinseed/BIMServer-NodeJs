81dec2b8131a88dc6696170fd20fbe0835858c7c
shinseed <307610991@qq.com>
2017-09-15 17:50:03 +0800

    base css

--- OpenBIMserver-nodejs/pages/login.vue
+++ OpenBIMserver-nodejs/pages/login.vue
@@ -1,16 +1,13 @@
-<style scoped>
 
-.title {
-    margin: 50px 0;
-}
-
-</style>
 
 <template>
 
-<section class="container">
-    <el-card class="box-card">
-        <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="100px" class="demo-dynamic">
+<section class="container"  >
+    <el-card class="box-card" style="width:400px;margin:0 auto">
+        <div slot="header" class="clearfix">
+            <span style="line-height: 36px;">登录</span>
+        </div>
+        <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="60px" class="demo-dynamic">
             <el-form-item prop="email" label="账号" :rules="[
           { required: true, message: '请输入邮箱地址', trigger: 'blur' },
           { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
@@ -20,10 +17,10 @@
             <el-form-item label="密码" prop="value" :rules="[
           { required: true, message: '密码不能为空', trigger: 'blur' }
         ]">
-                <el-input v-model="dynamicValidateForm.value"></el-input>
+                <el-input type='password' v-model="dynamicValidateForm.value"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="submitForm('dynamicValidateForm')">登录</el-button>
+                <el-button type="primary" @click="submitForm('dynamicValidateForm')">提交</el-button>
                 <el-button @click="resetForm('dynamicValidateForm')">注册</el-button>
             </el-form-item>
         </el-form>
@@ -35,23 +32,32 @@
 <script>
 
 export default {
-  asyncData ({ req }) {
-    return {
-      name: req ? 'server' : 'client'
-    }
-  },
-  head () {
-    return {
-        title: `Login Page (${this.name}-side)`
-    }
-  },
-    data() {
+    asyncData({
+            req
+        }) {
             return {
+                name: req ? 'server' : 'client',
                 dynamicValidateForm: {
                     value: '',
                     email: ''
                 }
-            };
+            }
+        },
+        head() {
+            return {
+                title: `Login Page (${this.name}-side)`
+            }
+        },
+        mounted() {
+            document.onkeydown = (event) => {
+                var e = event || window.event || arguments.callee.caller.arguments[0];
+                if (e && e.keyCode == 13) { // enter 键
+                    // if (this.$route.path == '/login') {
+                    this.submitForm('dynamicValidateForm')
+                        // }
+
+                }
+            }
         },
         methods: {
             submitForm(formName) {
